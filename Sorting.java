e1f5e191aba2ae833f422cadbc0d9ceeea9b0e8a
Tiago Oliveira <tiagopnoliveira@gmail.com>
2017-02-13 15:59:21 -0500

    added quickSort to sorting

--- Sorting.java
+++ Sorting.java
@@ -6,11 +6,13 @@ public class Sorting {
 
 	private static int[] a = {445,38,446,153,404,174,118,413,170,400,96,389,14,183,25,19,174,10,208,172,254,94,304,39,319,41,428,22,310,66,302,365,128,478,479,10,73,435,122,333,229,137,318,183,10,96,409,259,181,194,272,7};
 
+//	private static int[] a = {2,1,3};
+
     public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		// Core Function here
 		System.out.println("Before: " + Arrays.toString(a));
-		insertionSort(a);
+		quickSort(a);
 		System.out.println("After : " + Arrays.toString(a));
 		double duration = System.currentTimeMillis() - startTime;
 		System.out.println();
@@ -21,6 +23,9 @@ public class Sorting {
     }
     
     private static void swapElementsArray(int[] a, int p1, int p2) {
+		if(p1 == p2) {
+			return;
+		}
 		a[p1] ^= a[p2];
 		a[p2] ^= a[p1];
 		a[p1] ^= a[p2];
@@ -70,5 +75,50 @@ public class Sorting {
 
 	public static void mergeSort(int[] a) {
 		
+	}
+
+	public static void quickSort(int[] a) {
+		quickSort(a,0,a.length-1);
+	}
+	
+	private static void quickSort(int[] a, int s, int e) {
+		int p = partition(a,s,e);
+		if(s < p - 1) {
+			quickSort(a,s,p-1);
+		}
+		if(p < e) {
+			quickSort(a,p,e);
+		}
+	}
+	
+	private static int quickSortPivotSelect(int[] a, int s, int e) {
+		return e;
+	}
+	
+	private static int partition(int[] a, int s, int e) {
+		if(s >= e) {
+			return -1;
+		}
+		int pivotPos = quickSortPivotSelect(a,s,e);
+		if(pivotPos != e) {
+			swapElementsArray(a,pivotPos,e);
+		}
+		int l = s;
+		int r = e;
+		while(l <= r) {
+			while(a[l] < a[e]) {
+				l++;
+			}
+			while(a[r] > a[e]) {
+				r++;
+			}
+			if(l <= r) {
+				swapElementsArray(a,l,r);
+				l++;
+				r--;
+			}
+
+		}
+		return r+1;
 	}
 }
