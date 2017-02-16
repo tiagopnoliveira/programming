90b9685e85b0dbac58fe884af0016779eea838a6
Tiago Oliveira <oliveirat@MIAL18735.iis.amadeus.net>
2017-02-16 12:43:59 -0500

    added mergeSort to sorting

--- Sorting.java
+++ Sorting.java
@@ -2,22 +2,18 @@ import java.util.Arrays;
 
 public class Sorting {
 
-//	private static int[] a = {1,2,3};
+//	private static int[] a = {3,1,2};
 
 	private static int[] a = {445,38,446,153,404,174,118,413,170,400,96,389,14,183,25,19,174,10,208,172,254,94,304,39,319,41,428,22,310,66,302,365,128,478,479,10,73,435,122,333,229,137,318,183,10,96,409,259,181,194,272,7};
 
-//	private static int[] a = {7,2,4,6,1,3};
-
-	private static int c = 0;
+//	private static int[] a = {7,3,4,1,2};
 
     public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		// Core Function here
 		System.out.println("Before: " + Arrays.toString(a));
-		quickSort(a);
+		mergeSort(a);
 		System.out.println("After : " + Arrays.toString(a));
-		System.out.println(a.length);
-		System.out.println(c);
 		double duration = System.currentTimeMillis() - startTime;
 		System.out.println();
 		System.out.print("Processing time: ");
@@ -78,63 +74,79 @@ public class Sorting {
 	}
 
 	public static void mergeSort(int[] a) {
+		int t[] = new int[a.length];
+		mergeSort(a,t,0,a.length-1);
+	}
+	
+	private static void mergeSort(int[] a, int[] t, int s, int e) {
+		if(e <= s) {
+			return;
+		}
+		int p = ((e-s)/2) + s;
 		
+		mergeSort(a,t,s,p);
+		
+		if(p+1 <= e) {
+			mergeSort(a,t,p+1,e);
+		}				
+		
+		int l = s;
+		int r = p+1;
+		int c = s;
+		for(int i = s; i <= e; i++) {
+			t[i] = a[i];
+		}
+		
+		while(l <= p && r <= e) {
+			if(t[l] > t[r]) {
+				a[c] = t[r];
+				r++;
+			} else {
+				a[c] = t[l];
+				l++;
+			}
+			c++;
+		}
+		
+		while(l <= p) {
+			a[c] = t[l];
+			l++;
+			c++;
+		}
 	}
 
 	public static void quickSort(int[] a) {
-		c=0;
 		quickSort(a,0,a.length-1);
 	}
 	
-	private static void quickSort(int[] a, int s, int e) {
-        int l = s, r = e;
-        int p = a[quickSortPivotSelect(a,s,e)];
- 
-        /** partition **/
-        while (l <= r) {
-            while (a[l] < p) {
-                l++;
-			}
-            while (a[r] > p) {
-                r--;
-			}
-            if (l <= r) {
-				swapElementsArray(a,l,r);
-                l++;
-                r--;
-            }
-        }
- 
-        /** recursively sort lower half **/
-        if (s < r) {
-            quickSort(a,s,r);
-		}
-        /** recursively sort upper half **/
-        if (l < e) {
-            quickSort(a,l,e);
-		}
-	}
-	
 	private static int quickSortPivotSelect(int[] a, int s, int e) {
-		return s;
+		return e + ((s-e) / 2);
 	}
 	
-	private static int partition(int[] a, int s, int e) {
+	private static void quickSort(int[] a, int s, int e) {
+		int l = s;
+		int r = e;
 		int p = a[quickSortPivotSelect(a,s,e)];
-		while(s <= e) {
-			while(a[s] < p) {
-				s++;
+		
+		while(l <= r) {
+			while(a[l] < p) {
+				l++;
 			}
-			while(a[e] > p) {
-				e--;
+			while(a[r] > p) {
+				r--;
 			}
-			if(s <= e) {
-				swapElementsArray(a,s,e);
-				s++;
-				e--;
+			if(l <= r) {
+				swapElementsArray(a,l,r);
+				l++;
+				r--;
 			}
-
 		}
-		return e+1;
+		if(s < r) {
+			quickSort(a,s,r);
+		}
+		if(l < e) {
+			quickSort(a,l,e);
+		}
 	}
+	
 }
