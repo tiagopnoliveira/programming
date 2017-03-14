a4ea24f3c75340a4b58468157c5e8e6c3cce4e29
Tiago Oliveira <tiagopnoliveira@gmail.com>
2017-03-13 20:25:38 -0400

    fixed StringDecoderFB

--- StringDecoderFB.java
+++ StringDecoderFB.java
@@ -3,7 +3,7 @@ import java.util.*;
 // https://careercup.com/question?id=5187607743954944
 public class StringDecoderFB {
 	
-	private static final String s = "12222";
+	private static final String s = "001111771110112999";
 
     public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
@@ -24,15 +24,25 @@ public class StringDecoderFB {
 
     }
     
-    // WIP
     public static int stringDecoderFB(String s) {
 		int acc = 0;
-		int res = 0;
+		int res = 1;
+		int prevRes = 0;
+		boolean alreadyDoubled = false;
 		for(int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
 			int v = atoi(c);
-			if((acc*10 + v) <= 27) {
-				res++;
+			if((acc*10 + v) > 9 && (acc*10 + v) <= 27) {
+				int t = res;
+				if(alreadyDoubled) {
+					res += prevRes;
+				} else {
+					alreadyDoubled = true;
+					res *= 2;
+				}
+				prevRes = t;
+			} else {
+				alreadyDoubled = false;
 			}
 			acc = v;
 		}
@@ -57,7 +67,9 @@ public class StringDecoderFB {
 		if(acc > 27) {
 			return;
 		}
-		combinatorStringDecoderFB(s.substring(1,s.length()), r + (char) (acc-1 + 'a'), 0, res);
+		if(acc > 0) {
+			combinatorStringDecoderFB(s.substring(1,s.length()), r + (char) (acc-1 + 'a'), 0, res);
+		}
 		combinatorStringDecoderFB(s.substring(1,s.length()), r, acc*10, res);
 	}
 	
