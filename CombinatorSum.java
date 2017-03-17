5a30ec14a81fc88b1f6557534f327fbc6bd51954
Tiago Oliveira <oliveirat@MIAL18735.iis.amadeus.net>
2017-03-17 14:00:28 -0400

    added combinatorSum.java

---
+++ CombinatorSum.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+
+// https://careercup.com/question?id=5662358396469248
+public class CombinatorSum {
+
+	private static int[] a = {6,9,20};
+
+    public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		// Core Function here
+		System.out.println(combinatorSum(a,47));
+		double duration = System.currentTimeMillis() - startTime;
+		System.out.println();
+		System.out.print("Processing time: ");
+		System.out.format("%.3f", duration / 1000);
+		System.out.println(" seconds.");
+
+    }
+
+    public static boolean combinatorSum(int[] a, int n) {
+		HashSet<Integer> memo = new HashSet<Integer>();
+		return combinatorSum(a,n,memo);
+	}
+
+    
+    public static boolean combinatorSum(int[] a, int n, HashSet<Integer> memo) {
+		if(n < 0 || memo.contains(n)) {
+			return false;
+		}
+		if(n == 0) {
+			return true;
+		}
+		boolean res = false;
+		for(int i = 0; (i < a.length) && !res ; i++) {
+			res |= combinatorSum(a,n-a[i]);
+		}
+		if(!res) {
+			memo.add(n);
+		}
+		return res;
+	}
+
+}
