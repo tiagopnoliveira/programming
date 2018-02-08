2a358147d049dd654ca3f08bffe12bcd35ab2f24
Tiago Oliveira <tiago.oliveira@amadeus.com>
2018-02-08 16:29:50 -0500

    fixed class name to match file name

--- BSTImpl.java
+++ BSTImpl.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BSTImplMk2 {
+public class BSTImpl {
 	private static class IllegalBSTAccessException extends Exception {
 		public IllegalBSTAccessException() {
 			super();
@@ -253,12 +253,12 @@ public class BSTImplMk2 {
 //	private static Integer[] a = {445,38,446,153,404,174,118,413,170,400,96,389,14,183,25,19,174,10,208,172,254,94,304,39,319,41,428,22,310,66,302,365,128,478,479,10,73,435,122,333,229,137,318,183,10,96,409,259,181,194,272,7,212,464,446,163,37,209,151,155,21,84,171,421,153,464,428,195,15,44};
 //	private static Integer[] a = {50,35,28,12,32,14,4,68,41,21,7,55,16,1};
 //	private static Integer[] a = {50,35,28,12};
-	private static Integer[] a = {7,3,8,1,4};
+//	private static Integer[] a = {7,3,8,1,4};
 
     public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		// Core Function here
-		//BST<Integer> bst = new BST<Integer>(a);
+		//~ BST<Integer> bst = new BST<Integer>(a);
 		BST<Integer> bst = new BST<Integer>();
 		bst.insert(6);
 		bst.insert(5);
