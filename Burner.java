0f4f592d5415f619d923efe2aacc0f0c1efe48b1
Tiago Oliveira <tiago.oliveira@amadeus.com>
2018-02-12 18:03:45 -0500

    empty burner.java

--- Burner.java
+++ Burner.java
@@ -6,13 +6,11 @@ public class Burner {
     public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		// Core Function here
-				
+
 		double duration = System.currentTimeMillis() - startTime;
 		System.out.println();
 		System.out.print("Processing time: ");
 		System.out.format("%.3f", duration / 1000);
 		System.out.println(" seconds.");
     }
-
-
 }
