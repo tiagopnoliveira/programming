bf1fb6aadffef49db41556da475682510114acfa
Tiago Oliveira <tiago.oliveira@amadeus.com>
2018-02-12 17:38:56 -0500

    added empty function for time constrainted solving exercises

---
+++ Burner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Burner {
+
+    public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		// Core Function here
+				
+		double duration = System.currentTimeMillis() - startTime;
+		System.out.println();
+		System.out.print("Processing time: ");
+		System.out.format("%.3f", duration / 1000);
+		System.out.println(" seconds.");
+    }
+
+
+}
